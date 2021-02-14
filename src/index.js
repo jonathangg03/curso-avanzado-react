4dd3276f793c5678ecefd522f94da93e4f5aedf8
Jonathan Garcia <jona03g97@gmail.com>
2021-02-13 19:35:43 -0600

    Instalación de React y Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('empezamos')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Avanzando con el curso</h1>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,23 @@ module.exports = {
     output: {
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                       presets: ['@babel/preset-env', '@babel/preset-react'] 
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        })
     ]
 }
