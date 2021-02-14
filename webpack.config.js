4a8422369711b16890068acf2a30dac7f0279a6d
Jonathan Garcia <jona03g97@gmail.com>
2021-02-13 22:41:59 -0600

    Linter, extensiones y deploy con Vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const { Module } = require("webpack");
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    output: {
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js|jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                       presets: ['@babel/preset-env', '@babel/preset-react'] 
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-        })
+  output: {
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
