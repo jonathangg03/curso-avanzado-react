caec1f97a188039dca741ab9b6710780ff236ff6
Jonathan Garcia <jona03g97@gmail.com>
2021-02-14 21:40:05 -0600

    Creando animaciones con keyframes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
 
 const App = () => (
