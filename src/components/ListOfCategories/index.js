ecdd590fe34ab6cdd8c907a848dcb6d5af88b72b
Jonathan Garcia <jona03g97@gmail.com>
2021-02-16 17:05:13 -0600

    ¿Qué son los Hooks?

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
-export const ListOfCategories = () => (
-  <List>
-    {
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    window.fetch('https://petgram-server-khaki-psi.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+  return (
+    <List>
+      {
       categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
     }
-  </List>
-)
+    </List>
+  )
+}
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { ImgWrapper, Img, Buttons } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => (
   <div>
