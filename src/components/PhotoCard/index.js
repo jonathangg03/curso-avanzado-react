be370c7edecf16ee7b4545a672419b5e538d1f6b
Jonathan Garcia <jona03g97@gmail.com>
2021-02-16 21:28:48 -0600

    Uso de polyfill de Intersection Observer e imports dinámicos

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -9,15 +9,22 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        console.log('si')
-        setShow(true)
-        observer.disconnect()// Cuando el elemento ya este en el viewport del usuario, deje de escuchar, para que ya se quede ahí para siempre
-      }
-    })
-    observer.observe(element.current)
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    )
+      .then(() => {
+        const observer = new window.IntersectionObserver((entries) => {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            console.log('si')
+            setShow(true)
+            observer.disconnect()// Cuando el elemento ya este en el viewport del usuario, deje de escuchar, para que ya se quede ahí para siempre
+          }
+        })
+        observer.observe(element.current)
+      })
   }, [element])
   return (
     <Article ref={element}>
