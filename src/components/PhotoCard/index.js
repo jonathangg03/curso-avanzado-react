adb769cf6d0f46e01ee1f9b33e75a0d6c881a762
Jonathan Garcia <jona03g97@gmail.com>
2021-02-16 19:22:28 -0600

    Usando Intersection Observer

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,18 +1,39 @@
-import React from 'react'
-import { ImgWrapper, Img, Buttons } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Buttons, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => (
-  <div>
-    <a href={`/detail/${id}`}>
-      <ImgWrapper>
-        <Img src={src} />
-      </ImgWrapper>
-    </a>
-    <Buttons>
-      <MdFavoriteBorder size='32px' />{likes} likes!
-    </Buttons>
-  </div>
-)
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null)
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) {
+        console.log('si')
+        setShow(true)
+        observer.disconnect()// Cuando el elemento ya este en el viewport del usuario, deje de escuchar, para que ya se quede ahí para siempre
+      }
+    })
+    observer.observe(element.current)
+  }, [element])
+  return (
+    <Article ref={element}>
+      {
+        show &&
+          <>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+            <Buttons>
+              <MdFavoriteBorder size='32px' />{likes} likes!
+            </Buttons>
+          </>
+      }
+    </Article>
+  )
+}
