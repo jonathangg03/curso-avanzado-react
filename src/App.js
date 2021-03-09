d0ab4e02774defc334fb1689ac0ab6112965f29c
Jonathan Garcia <jona03g97@gmail.com>
2021-03-08 23:18:48 -0600

    Creando la página Detail

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react'
 import { Router } from '@reach/router'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { Logo } from './components/Logo'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 
 const App = () => {
-  const urlPrams = new window.URLSearchParams(window.location.search)
-  const detailId = urlPrams.get('detail')
-  console.log(detailId)
   return (
     <div>
       <GlobalStyles />
       <Logo />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
+      <Router>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+      </Router>
     </div>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from '@reach/router'
 import { ImgWrapper, Img, Article } from './styles'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
@@ -16,11 +17,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
       {
         show &&
           <>
-            <a href={`/?detail=${id}`}>
+            <Link to={`/detail/${id}`}>
               <ImgWrapper>
                 <Img src={src} />
               </ImgWrapper>
-            </a>
+            </Link>
             <ToggleLikeMutation>
               {
                 (toggleLike) => {
