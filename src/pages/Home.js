765804736c0a209fd04c9506643df326792c41e3
Jonathan Garcia <jona03g97@gmail.com>
2021-03-21 00:58:30 -0600

    React Helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCard } from '../container/ListOfPhotocards'
+import { Layout } from '../components/Layout'
 
 export const Home = ({ id }) => {
   return (
-    <>
+    <Layout title='Tu app de fotos de mascotas' subtitle='Con Petgranm puedes ver fotos de animales domesticos muy bonitas'>
       <ListOfCategories />
       <ListOfPhotoCard categoryId={id} />
-    </>
+    </Layout>
   )
 }
